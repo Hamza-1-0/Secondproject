f9b8f10916b7f40879be1c2a4e5a3bd584ba0f6a
hamzaelprens8@gmail.com <hamzaelprens8@gmail.com>
2024-04-22 00:30:45 +0200

    Add header

--- src/Components/Choose/Choose.jsx
+++ src/Components/Choose/Choose.jsx
@@ -1,7 +1,96 @@
 import React from "react";
 import "./Choose-style.css";
+import { Container, Row, Col } from "react-bootstrap";
 const Choose = () => {
-  return <div>Choose</div>;
+  return (
+    <div className="choose">
+      <h2>Why Choose Us</h2>
+      <p>Lorem ipsum dolor sit amet</p>
+      <Container className="d-flex">
+        <Row className="cont">
+          <Col>
+            <div className="text-choose">
+              <h4>Experience</h4>
+              <p>
+                Ronsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div className="icon">
+              <img src="src\assets\News\icons\icon-1.svg" alt="" />
+            </div>
+          </Col>
+          <Col>
+            <div className="text-choose">
+              <h4>Products</h4>
+              <p>
+                Ronsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div className="icon">
+              <img src="src\assets\News\icons\icon-2.svg" alt="" />
+            </div>
+          </Col>
+          <Col>
+            <div className="text-choose">
+              <h4>Approach</h4>
+              <p>
+                Ronsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div className="icon">
+              <img src="src\assets\News\icons\icon-3.svg" alt="" />
+            </div>
+          </Col>
+        </Row>
+        <Row className="cont">
+          <img src="src\assets\News\features.jpg" alt="" className="mid" />
+        </Row>
+        <Row className="cont">
+          <Col>
+            {" "}
+            <div className="icon">
+              <img src="src\assets\News\icons\icon-4.svg" alt="" />
+            </div>
+            <div className="text-choose">
+              <h4>Pricing</h4>
+              <p>
+                Ronsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+          </Col>
+          <Col>
+            {" "}
+            <div className="icon">
+              <img src="src\assets\News\icons\icon-5.svg" alt="" />
+            </div>
+            <div className="text-choose">
+              <h4>Delivery</h4>
+              <p>
+                Ronsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <div className="icon">
+              <img src="src\assets\News\icons\icon-6.svg" alt="" />
+            </div>{" "}
+            <div className="text-choose">
+              <h4>Approach</h4>
+              <p>
+                Ronsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Choose;
