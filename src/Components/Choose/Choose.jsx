50a63ec90b205002ebaafaccb26b6828aa8d67d5
hamzaelprens8@gmail.com <hamzaelprens8@gmail.com>
2024-04-21 23:52:29 +0200

    Add header

---
+++ src/Components/Choose/Choose.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Choose-style.css";
+const Choose = () => {
+  return <div>Choose</div>;
+};
+
+export default Choose;
--- src/Screen/Home.jsx
+++ src/Screen/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Header from "../Components/Header/Header";
 import Information from "../Components/Information/Information";
+import Choose from "../Components/Choose/Choose";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Header />
       <Information />
+      <Choose />
     </>
   );
 };
