1b850c21b83e3783d1cd94c71961baec01eab9d9
hamzaelprens8@gmail.com <hamzaelprens8@gmail.com>
2024-04-24 06:13:40 +0200

    Add header

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,280 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./Testimonials-style.css";
+import { Pagination } from "swiper/modules";
+import { IoStarSharp } from "react-icons/io5";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
+import { Container } from "react-bootstrap";
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials">
+      <h2 className="text-top">Testimonials</h2>
+      <p className="text-down">Lorem ipsum dolor sit amet</p>
+
+      <Container>
+        {" "}
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="card-test">
+              <div className="content-top">
+                <img
+                  src="src\assets\News\testimonial\testimonial-1.jpg"
+                  alt=""
+                />
+                <div className="content-text">
+                  <h3>John Doe</h3>
+                  <p>CFO</p>
+                  <div className="stars">
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                  </div>
+                </div>
+              </div>
+              <div className="content-down">
+                <p>
+                  {" "}
+                  <FaQuoteLeft />
+                  Proin iaculis purus consequat sem cure digni ssim donec
+                  porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                  eget id, aliquam eget nibh et. Maecen aliquam.
+                  <FaQuoteRight />
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="card-test">
+              <div className="content-top">
+                <img
+                  src="src\assets\News\testimonial\testimonial-2.jpg"
+                  alt=""
+                />
+                <div className="content-text">
+                  <h3>Afa Rose</h3>
+                  <p>Web Designer</p>
+                  <ul className="stars">
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="content-down">
+                <p>
+                  {" "}
+                  <FaQuoteLeft />
+                  Proin iaculis purus consequat sem cure digni ssim donec
+                  porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                  eget id, aliquam eget nibh et. Maecen aliquam.
+                  <FaQuoteRight />
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="card-test">
+              <div className="content-top">
+                <img
+                  src="src\assets\News\testimonial\testimonial-3.jpg"
+                  alt=""
+                />
+                <div className="content-text">
+                  <h3>Keena lara</h3>
+                  <p>Store Owner</p>
+                  <ul className="stars">
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="content-down">
+                <p>
+                  {" "}
+                  <FaQuoteLeft />
+                  Proin iaculis purus consequat sem cure digni ssim donec
+                  porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                  eget id, aliquam eget nibh et. Maecen aliquam.
+                  <FaQuoteRight />
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="card-test">
+              <div className="content-top">
+                <img
+                  src="src\assets\News\testimonial\testimonial-4.jpg"
+                  alt=""
+                />
+                <div className="content-text">
+                  <h3>Fizzi Prandon</h3>
+                  <p>Freelancer</p>
+                  <ul className="stars">
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="content-down">
+                <p>
+                  <FaQuoteLeft />
+                  Proin iaculis purus consequat sem cure digni ssim donec
+                  porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                  eget id, aliquam eget nibh et. Maecen aliquam.
+                  <FaQuoteRight />
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="card-test">
+              <div className="content-top">
+                <img
+                  src="src\assets\News\testimonial\testimonial-1.jpg"
+                  alt=""
+                />
+                <div className="content-text">
+                  <h3>John Doe</h3>
+                  <p>CFO</p>
+                  <ul className="stars">
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="content-down">
+                <p>
+                  {" "}
+                  <FaQuoteLeft />
+                  Proin iaculis purus consequat sem cure digni ssim donec
+                  porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                  eget id, aliquam eget nibh et. Maecen aliquam.
+                  <FaQuoteRight />
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="card-test">
+              <div className="content-top">
+                <img
+                  src="src\assets\News\testimonial\testimonial-2.jpg"
+                  alt=""
+                />
+                <div className="content-text">
+                  <h3>Afa Rose</h3>
+                  <p>Web Designer</p>
+                  <ul className="stars">
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                    <li>
+                      <IoStarSharp />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="content-down">
+                <p>
+                  <FaQuoteLeft /> Proin iaculis purus consequat sem cure digni
+                  ssim donec porttitora entum suscipit rhoncus. Accusantium
+                  quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam.{" "}
+                  <FaQuoteRight />
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </Container>
+    </div>
+  );
+};
+
+export default Testimonials;
--- src/Screen/Home.jsx
+++ src/Screen/Home.jsx
@@ -5,6 +5,7 @@ import Information from "../Components/Information/Information";
 import Choose from "../Components/Choose/Choose";
 import Portfolio from "../Components/Portfolio/Portfolio";
 import Pricing from "../Components/Pricing/Pricing";
+import Testimonials from "../Components/Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Choose />
       <Portfolio />
       <Pricing />
+      <Testimonials />
     </>
   );
 };
