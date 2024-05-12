8e71d6f47d4021e29aae01d5082dbc324297eab7
hamzaelprens8@gmail.com <hamzaelprens8@gmail.com>
2024-05-12 17:54:29 +0300

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
 import Home from "./Screen/Home";
+import Services from "./Screen/Services";
+import Portfolio from "./ScreenPortfolio";
+import Testimonials from "./ScreenTestimonials";
+import Team from "./Screen/Team";
+import Menu from "./Screen/Menu";
+import News from "./Screen/News";
 import "./App.css";
 const App = () => {
   return (
     <>
       <Home />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Team />
+      <Menu />
+      <News />
     </>
   );
 };
